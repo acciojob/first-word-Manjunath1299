defa5022def5ae13d77e86348016c55b55bc6d63
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2024-01-07 18:48:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function firstWord(s) {
   // your code here
+  // console.log(s);
+  s=s.split(" ");
+  if(s.length==1){
+  return s[0]
+  }
+  else if(s.length<1){
+  return " "
+  }
+  else if(s.length>1){
+  return s[0];
+  }
 }
 
 // Do not change the code below
